c59f449590b106bd8c4f400164542c4f529df0d6
ihsan putra riadi <iputrariadi@gmail.com>
2023-05-31 15:10:51 +0700

    Menambahkan konstruktor untuk kelas Orang dengan inisialisasi umur

---
+++ ParadigmaOOP2_041/ParadigmaOOP2_041.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class orang {
+public:
+	int umur;
+
+	orang(int pumur) :
+		umur(pumur)
+	{
+		cout << "orang dibuat dengan umur " << umur << "\n" << endl;
+	}
+};
+
