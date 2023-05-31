b1a00e00a93e6434b4a6dd60ca63cbee6b5494b7
ihsan putra riadi <iputrariadi@gmail.com>
2023-05-31 16:42:48 +0700

    Mendefinisikan fungsi perkenalan() pada baseClass

--- Program noOverriding_041/Program noOverriding_041.cpp
+++ Program noOverriding_041/Program noOverriding_041.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 class baseClass {
 public:
-	virtual void perkenalan()final {
+	virtual void Perkenalan()final {
 		cout << "Hallo saya Function dari base class";
 	}
 };
 
-class derivedClass : public baseClass {
+class derivedClass :public baseClass {
 public:
 	void perkenalan() {
 		cout << "Hallo saya Function dari derived Class";
