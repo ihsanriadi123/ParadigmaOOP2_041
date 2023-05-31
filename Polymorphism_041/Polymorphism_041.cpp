9c20a513f1fa7dea6901bf47f0e6375a63fdb7eb
ihsan putra riadi <iputrariadi@gmail.com>
2023-05-31 16:23:08 +0700

    Menambahkan kelas Joko yang mewarisi kelas Seseorang

--- Polymorphism_041/Polymorphism_041.cpp
+++ Polymorphism_041/Polymorphism_041.cpp
@@ -13,3 +13,19 @@ public:
 	}
 };
 
+class joko :public seseorang {
+public:
+	void pesan() {
+		cout << "Pesan dari joko" << endl;
+	}
+};
+
+class lia : public seseorang {
+public:
+	void pesan() {
+		cout << "pesan dari lia" << endl;
+	}
+
+
+};
+
