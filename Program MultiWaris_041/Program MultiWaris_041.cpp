00555114bdfdb86e5451dc2f2e995f5fc0c9858e
ihsan putra riadi <iputrariadi@gmail.com>
2023-05-31 17:01:16 +0700

    Menggunakan inheritance private dari class manusia dan public dari class orang pada class pelajar

--- Program MultiWaris_041/Program MultiWaris_041.cpp
+++ Program MultiWaris_041/Program MultiWaris_041.cpp
@@ -28,3 +28,21 @@ public:
 	}
 };
 
+class pelajar : private manusia, public orang {
+public:
+	string sekolah;
+
+	pelajar(string pNama, string pJenisKelamin, string pSekolah) :
+		orang(pNama),
+		manusia(pJenisKelamin),
+		sekolah(pSekolah) {
+		cout << "pelajar dibuat\n" << endl;
+	}
+	~pelajar() {
+		cout << "pelajar dihapus\n" << endl;
+	}
+	string perkenalan() {
+		return "Hallo, nama saya" + nama + "dengan jenis kelam" + jeniskelamin + "dari sekolah" + sekolah + "\n\n";
+	}
+};
+
